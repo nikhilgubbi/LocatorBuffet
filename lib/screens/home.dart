3fd548d926ae57af55c6ead6e3bbd15e4119bd81
Vince <vince@tchissekevicente.com>
2020-06-28 00:04:25 +0200

    Card widget
    Implementing the card widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
+import 'dart:collection';
 
+import 'package:buffetlocator/utilities/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:buffetlocator/api_key.dart' as api_key;
@@ -10,25 +12,88 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  GoogleMapController mapController;
+  GoogleMapController _controller;
+  List<Marker> _myMarkers = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _myMarkers.add(Marker(
+        markerId: MarkerId("1"),
+        infoWindow: InfoWindow(title: Constants.LANDMARK_ONE),
+        draggable: false,
+        position: Constants.MARKER_ONE));
+
+    _myMarkers.add(Marker(
+        markerId: MarkerId("2"),
+        infoWindow: InfoWindow(title: Constants.LANDMARK_TWO),
+        draggable: false,
+        position: Constants.MARKER_TWO));
+
+    _myMarkers.add(Marker(
+        markerId: MarkerId("3"),
+        infoWindow: InfoWindow(title: Constants.LANDMARK_THREE),
+        draggable: false,
+        position: Constants.MARKER_THREE));
+
+    _myMarkers.add(Marker(
+        markerId: MarkerId("4"),
+        infoWindow: InfoWindow(title: Constants.LANDMARK_FOUR),
+        draggable: false,
+        position: Constants.MARKER_FOUR));
+
+    _myMarkers.add(Marker(
+        markerId: MarkerId("5"),
+        infoWindow: InfoWindow(title: Constants.LANDMARK_FIVE),
+        draggable: false,
+        position: Constants.MARKER_FIVE));
+  }
+
+  void _onMapCreated(contoller) {
+    setState(() {
+      _controller = contoller;
+      _controller.setMapStyle(MAP_STYLE);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(
-            target: LatLng(37.42796133580664, -122.085749655962),
-          ),
-          onMapCreated:  _onMapCreated,
-        ),
+      body: new Stack(
+        children: <Widget>[
+          new Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: GoogleMap(
+                myLocationEnabled: true,
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                    target: Constants.CURRENT_LOCATION, zoom: 10.0),
+                markers: Set.from(_myMarkers),
+              )),
+          Positioned(
+              top: MediaQuery.of(context).size.height - 230.0,
+              child: Wrap(
+                children: <Widget>[
+                  Card(
+                      margin: EdgeInsets.only(
+                          top: 10, left: 10, right: 10, bottom: 10),
+                      elevation: 8,
+                      child: new Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Container(
+                            height: MediaQuery.of(context).size.height - 450.0,
+                            width: MediaQuery.of(context).size.width - 40.0,
+                            child: Wrap(
+                                alignment: WrapAlignment.center,
+                                crossAxisAlignment: WrapCrossAlignment.center,
+                                children: <Widget>[])),
+                      )),
+                ],
+              )),
+        ],
       ),
     );
-  }
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-    mapController.setMapStyle(MAP_STYLE);
   }
 }
---
+++ lib/utilities/constants.dart
@@ -0,0 +1,16 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Constants {
+  static const TITLE = "Buffet Locator",
+      CURRENT_LOCATION = LatLng(40.69, -74.25),
+      MARKER_ONE = LatLng(40.59, -74.15),
+      MARKER_TWO = LatLng(40.79, -74.35),
+      MARKER_THREE = LatLng(40.63, -74.29),
+      MARKER_FOUR = LatLng(40.72, -74.31),
+      MARKER_FIVE = LatLng(40.61, -74.35),
+      LANDMARK_ONE = "NAME 1",
+      LANDMARK_TWO = "NAME 2",
+      LANDMARK_THREE = "NAME 3",
+      LANDMARK_FOUR = "NAME 4",
+      LANDMARK_FIVE = "NAME 5";
+}
