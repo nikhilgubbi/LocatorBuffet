f1111534153ff434ada3797c252bd3672c7d4df1
Shivam Singhania <30895841+ShivamSinghania@users.noreply.github.com>
2020-06-27 22:08:18 +0530

    Initial setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# buffetlocator
+
+An app to find & place food around your city.
+
+To run the app on android : https://pub.dev/packages/google_maps_flutter#android
---
+++ android/app/src/main/java/shivam/testing/buffetlocator/MainActivity.java
@@ -0,0 +1,6 @@
+package shivam.testing.buffetlocator;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
